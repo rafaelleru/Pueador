dc5d026222802878d705ff709d622adbb728dbde
JorgeSG <jorgesanchezgr@gmail.com>
2016-01-19 18:24:26 +0100

    Menu acabado

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
+import os
+
+os.system('clear')
 
 print "Escoja una opcion:\n"
-print "\t 1. Opcion 1.\n"
-print "\t 2. Opcion 2.\n"
-print "\t 3. Opcion 3.\n"
+print "\t 1. Bomba fork.(Solo en Linux)\n"
+print "\t 2. Cagetizador.\n"
+
+
+
+print "\t 3. Cambiar alias.\n"
+print "\t 4. Trolling.\n"
+print "\t 5. Llenar home de basura.\n"
+#print "\t 6. Quijotizador.\n"
 
 opcion = input('Introduzca una opcion:')
 
 if opcion == '1':
-    print 'Ejecuta opcion 1'
+    print 'Ejecuta Bomba fork'
 elif opcion == '2':
-    print 'Ejecuta opcion 2'
+    print 'Ejecuta Cagetizador'
 elif opcion == '3':
-    print 'Ejecuta opcion 3'
+    print 'Ejecuta Cambiar alias'
+elif opcion == '4':
+    print 'Ejecuta Trolling'
+elif opcion == '5':
+    print 'Ejecuta llenar home de basura'
+else:
+    print 'Opcion no valida.'
