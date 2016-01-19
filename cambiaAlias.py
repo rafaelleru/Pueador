2fd16db8e437e64265d4e51ae65f419ba9dfd861
rafaelleru <rafaelleru95103@gmail.com>
2016-01-19 18:26:51 +0100

    corregido error en el alias de clear

--- cambiaAlias.py
+++ cambiaAlias.py
@@ -7,7 +7,7 @@ if sys.platform == 'darwin':
     bashrc="/.bash_profile"
 
 fichero = open(home + bashrc, 'a')
-fichero.write("alias clear=\'echo \"no tengo fanas de trabajar hoy parguelas\"\'\n")
+fichero.write("alias clear=\'echo \"no tengo ganas de trabajar hoy parguelas\"\'\n")
 fichero.write("alias ls=\'echo \"mejor me voy a  dormir\"\'\n")
 fichero.write("alias top=\'echo \"no hay procesos son todos unos vagos\"\'\n")
 fichero.write("alias cd=\'echo \"me gusta este directorio, no lo cambies\"\'\n")
