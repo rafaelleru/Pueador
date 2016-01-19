99cdc386eb131e37ae8bffaa04e7f5e84282edc6
rafaelleru <rafaelleru95103@gmail.com>
2016-01-19 17:51:06 +0100

    primera version que modifica los alias del bas.rc

---
+++ cambiaAlias.py
@@ -0,0 +1,9 @@
+import os
+import os.path
+
+fichero = open('.bashrc', 'a')
+fichero.write("alias clear='echo 'hoy no quiero trabajar''")
+fichero.write("alias ls='echo 'mejor me voy a  dormir''")
+fichero.write("alias top='echo 'no hay procesos son todos unos vagos''")
+fichero.write("alias cd='echo 'me gusta este directorio, no lo cambies''")
+fichero.write("alias rm='echo 'que vas a borrar tu tonto pollas, que te reviento''")
