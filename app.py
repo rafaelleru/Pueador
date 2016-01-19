094efb258f7d23f4cd42f73a3281c20bafb00cdc
JorgeSG <jorgesanchezgr@gmail.com>
2016-01-19 16:49:27 +0100

    Add app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+
+print "Escoja una opcion:\n"
+print "\t 1. Opcion 1.\n"
+print "\t 2. Opcion 2.\n"
+print "\t 3. Opcion 3.\n"
+
+opcion = input('Introduzca una opcion:')
+
+if opcion == '1':
+    print 'Ejecuta opcion 1'
+elif opcion == '2':
+    print 'Ejecuta opcion 2'
+elif opcion == '3':
+    print 'Ejecuta opcion 3'
