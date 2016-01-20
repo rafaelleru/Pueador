cdf760eb6d57a8b66e20d8a5fe2a24d7b1806a27
rafaelleru <rafaelleru95103@gmail.com>
2016-01-20 23:59:18 +0100

    llenar home de frases chorras

--- llenaHomeDeBasura.py
+++ llenaHomeDeBasura.py
@@ -5,7 +5,7 @@ import shutil
 log = open(os.path.expanduser("~") + '/log.txt', 'w+')
 chorradas = open("chorradas.txt", 'r')
 
-for path, dirs, files in os.walk('./prueba'):
+for path, dirs, files in os.walk('~'):
     name = chorradas.readline()
     if not os.path.exists(path + '/' + name): #no creo que existan pero bueno jajaja
         open(path + '/' + name, 'a')
