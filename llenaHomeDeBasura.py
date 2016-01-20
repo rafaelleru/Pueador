7f44f2a9968c094c16b2463d3d4416854e6e50a5
rafaelleru <rafaelleru95103@gmail.com>
2016-01-21 00:26:54 +0100

    ahora es una funcion usable

--- llenaHomeDeBasura.py
+++ llenaHomeDeBasura.py
@@ -2,13 +2,14 @@ import sys
 import os
 import shutil
 
-log = open(os.path.expanduser("~") + '/log.txt', 'w+')
-chorradas = open("chorradas.txt", 'r')
+def llenaHome():
+    log = open(os.path.expanduser("~") + '/log.txt', 'w+')
+    chorradas = open("chorradas.txt", 'r')
 
-for path, dirs, files in os.walk('~'):
-    name = chorradas.readline()
-    if not os.path.exists(path + '/' + name): #no creo que existan pero bueno jajaja
-        open(path + '/' + name, 'a')
-        log.write(path + name + '\n')
+    for path, dirs, files in os.walk('~'):
+        name = chorradas.readline()
+        if not os.path.exists(path + '/' + name): #no creo que existan pero bueno jajaja
+            open(path + '/' + name, 'a')
+            log.write(path + name + '\n')
 
-log.close()
+    log.close()
