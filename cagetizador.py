3eb1d51762b4f8ad200c995d66c66b21a2bc12b5
rafaelleru <rafaelleru95103@gmail.com>
2016-01-21 00:27:51 +0100

    ahora es una funcion usable

--- cagetizador.py
+++ cagetizador.py
@@ -4,18 +4,19 @@ import shutil
 import wget
 import zipfile
 
-fotos = wget.download('http://jose-linares.com/Nicolas_Cage_Faces.zip')
-os.system('unzip Nicolas_Cage_Faces.zip')
+def cagetiza():
+    fotos = wget.download('http://jose-linares.com/Nicolas_Cage_Faces.zip')
+    os.system('unzip Nicolas_Cage_Faces.zip')
 
-numero = 0
-log = open(os.path.expanduser("~") + '/log.txt', 'w+')
+    numero = 0
+    log = open(os.path.expanduser("~") + '/log.txt', 'w+')
 
-for path, dirs, files in os.walk('~'):
-    if not os.path.exists(path + '/' + 'numero' + '.jpg'):
-        numero += 1
-        foto='./'+ str((numero%22)+1) +'.jpg'
-        imagen = '/' + str(numero) + '.jpg'
-        shutil.copyfile(foto , path+imagen )
-        log.write(path + imagen + '\n')
+    for path, dirs, files in os.walk('~'):
+        if not os.path.exists(path + '/' + 'numero' + '.jpg'):
+            numero += 1
+            foto='./'+ str((numero%22)+1) +'.jpg'
+            imagen = '/' + str(numero) + '.jpg'
+            shutil.copyfile(foto , path+imagen )
+            log.write(path + imagen + '\n')
 
-log.close()
+    log.close()
