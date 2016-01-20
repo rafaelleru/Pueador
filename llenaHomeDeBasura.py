2ce9b7ddc2ac4cf7d84d0fba14e7a2905eb31cc9
rafaelleru <rafaelleru95103@gmail.com>
2016-01-21 00:32:37 +0100

    escoge linea random

--- llenaHomeDeBasura.py
+++ llenaHomeDeBasura.py
@@ -1,13 +1,14 @@
 import sys
 import os
 import shutil
+import random
 
 def llenaHome():
     log = open(os.path.expanduser("~") + '/log.txt', 'w+')
     chorradas = open("chorradas.txt", 'r')
 
-    for path, dirs, files in os.walk('~'):
-        name = chorradas.readline()
+    for path, dirs, files in os.walk('./prueba'):
+        name = random.choice(chorradas.readlines())
         if not os.path.exists(path + '/' + name): #no creo que existan pero bueno jajaja
             open(path + '/' + name, 'a')
             log.write(path + name + '\n')
