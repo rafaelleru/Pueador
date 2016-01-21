e979d234b5d90e64ebf10854c1168a3a5149565a
rafaelleru <rafaelleru95103@gmail.com>
2016-01-21 01:22:36 +0100

    Ahora aunque se acabe el archivo no para

--- llenaHomeDeBasura.py
+++ llenaHomeDeBasura.py
@@ -3,15 +3,23 @@ import os
 import shutil
 import random
 
+chorradas
+
 def llenaHome():
     log = open(os.path.expanduser("~") + '/log.txt', 'w+')
-    chorradas = open("chorradas.txt", 'r')
+    openChorradas()
 
     for path, dirs, files in os.walk('./prueba'):
         for i in range(1,6):
             name = random.choice(chorradas.readlines())
+            if name == '':
+                chorradas.close()
+                openChorradas()
             if not os.path.exists(path + '/' + name): #no creo que existan pero bueno jajaja
                 open(path + '/' + name, 'a')
                 log.write(path + name + '\n')
 
     log.close()
+
+def openChorradas():
+    chorradas = open("chorradas.txt", 'r')
