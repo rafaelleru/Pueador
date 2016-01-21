f9f758ceecd264203cb13b4f70a6b3458f6432b6
rafaelleru <rafaelleru95103@gmail.com>
2016-01-21 13:46:21 +0100

    corregido

--- llenaHomeDeBasura.py
+++ llenaHomeDeBasura.py
@@ -3,23 +3,18 @@ import os
 import shutil
 import random
 
-chorradas
-
 def llenaHome():
+    chorradas = open(os.path.expanduser("~") + "/Puteador/chorradas.txt", 'r')
     log = open(os.path.expanduser("~") + '/log.txt', 'w+')
-    openChorradas()
-
     for path, dirs, files in os.walk('./prueba'):
         for i in range(1,6):
-            name = random.choice(chorradas.readlines())
+            name = chorradas.readline()
             if name == '':
                 chorradas.close()
-                openChorradas()
-            if not os.path.exists(path + '/' + name): #no creo que existan pero bueno jajaja
-                open(path + '/' + name, 'a')
+                chorradas = open(os.path.expanduser("~") + "/Puteador/chorradas.txt", 'r')
+            if not os.path.exists(path + '/' + name + '.txt'): #no creo que existan pero bueno jajaja
+                open(path + '/' + name + '.txt', 'a')
                 log.write(path + name + '\n')
 
     log.close()
-
-def openChorradas():
-    chorradas = open("chorradas.txt", 'r')
+    return
